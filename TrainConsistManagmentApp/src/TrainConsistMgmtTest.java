23c503753681c41d383c6b9a02ecc26f9245219e
mohith1402 <ballolamohith1402@gmail.com>
2026-04-24 14:06:59 +0530

    UC16: Sort Passenger Bogies by Capacity (Bubble Sort – Algorithm Intro)

--- TrainConsistManagmentApp/src/TrainConsistMgmtTest.java
+++ TrainConsistManagmentApp/src/TrainConsistMgmtTest.java
@@ -1,78 +1,68 @@
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertArrayEquals;
 
 public class TrainConsistMgmtTest {
 
-    @Test
-    public void testCargo_SafeAssignment() {
-        GoodsBogie bogie = new GoodsBogie("Cylindrical");
-        try {
-            bogie.assignCargo("Petroleum");
-            assertEquals("Petroleum", bogie.cargo);
-        } catch (CargoSafetyException e) {
-            fail("Exception should not be thrown for safe assignment");
+    private void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
         }
     }
 
     @Test
-    public void testCargo_UnsafeAssignmentHandled() {
-        GoodsBogie bogie = new GoodsBogie("Rectangular");
-        try {
-            bogie.assignCargo("Petroleum");
-            fail("CargoSafetyException should have been thrown");
-        } catch (CargoSafetyException e) {
-            assertTrue(e.getMessage().contains("Cannot assign Petroleum to a Rectangular bogie"));
-        }
+    public void testSort_BasicSorting() {
+        int[] capacities = {72, 56, 24, 70, 60};
+        int[] expected = {24, 56, 60, 70, 72};
+
+        bubbleSort(capacities);
+
+        assertArrayEquals(expected, capacities);
     }
 
     @Test
-    public void testCargo_CargoNotAssignedAfterFailure() {
-        GoodsBogie bogie = new GoodsBogie("Rectangular");
-        try {
-            bogie.assignCargo("Petroleum");
-        } catch (CargoSafetyException e) {
-            // Exception is expected and handled
-        }
-        assertNull(bogie.cargo);
+    public void testSort_AlreadySortedArray() {
+        int[] capacities = {24, 56, 60, 70, 72};
+        int[] expected = {24, 56, 60, 70, 72};
+
+        bubbleSort(capacities);
+
+        assertArrayEquals(expected, capacities);
     }
 
     @Test
-    public void testCargo_ProgramContinuesAfterException() {
-        GoodsBogie invalidBogie = new GoodsBogie("Rectangular");
-        GoodsBogie validBogie = new GoodsBogie("Open");
+    public void testSort_DuplicateValues() {
+        int[] capacities = {72, 56, 56, 24};
+        int[] expected = {24, 56, 56, 72};
 
-        try {
-            invalidBogie.assignCargo("Petroleum"); // This will fail
-        } catch (CargoSafetyException e) {
-            // Handled safely
-        }
+        bubbleSort(capacities);
 
-        try {
-            // Program continues and executes next assignment
-            validBogie.assignCargo("Coal");
-        } catch (CargoSafetyException e) {
-            fail("Should not throw exception here");
-        }
-
-        assertEquals("Coal", validBogie.cargo);
+        assertArrayEquals(expected, capacities);
     }
 
     @Test
-    public void testCargo_FinallyBlockExecution() {
-        GoodsBogie bogie = new GoodsBogie("Rectangular");
-        boolean finallyExecuted = false;
+    public void testSort_SingleElementArray() {
+        int[] capacities = {50};
+        int[] expected = {50};
 
-        try {
-            bogie.assignCargo("Petroleum");
-        } catch (CargoSafetyException e) {
-            // Handled safely
-        } finally {
-            finallyExecuted = true;
-        }
+        bubbleSort(capacities);
 
-        assertTrue(finallyExecuted);
+        assertArrayEquals(expected, capacities);
+    }
+
+    @Test
+    public void testSort_AllEqualValues() {
+        int[] capacities = {40, 40, 40};
+        int[] expected = {40, 40, 40};
+
+        bubbleSort(capacities);
+
+        assertArrayEquals(expected, capacities);
     }
 }
